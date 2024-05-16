5f695a43ad03c8476d2dfa039bb38bd341400dee
Schvefgwr1 <92165301+Schvefgwr1@users.noreply.github.com>
2024-05-16 21:38:53 +0300

    Initial commit

---
+++ src/main/java/test/ttttt/CrptApi.java
@@ -0,0 +1,2 @@
+package test.ttttt;public class CrptApi {
+}
---
+++ src/test/java/test/ttttt/TttttApplicationTests.java
@@ -0,0 +1,13 @@
+package test.ttttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
